5d73cf1e9b47f1da6f26e33a2da1d8d42a3d79e2
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-22 14:48:01 +0300

    роутинг главной страницы для отображения веток

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,20 @@
-// const app = require('../src/App.js');
 const express = require('express');
+const App = require('./../app');
+const { repoName } = require('./../configs');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', { mainTitle: 'Mega GIT', pageTitle: 'The Modern Digital IT Product' });
+router.get('/', (req, res, next) => {
+  App.getAllBranches()
+    .then((data) => {
+      res.render('index', {
+        mainTitle: 'Mega GIT',
+        pageTitle: 'The Modern Digital IT Product',
+        repoName,
+        branchArr: data
+      });
+    })
+    .catch((error) => { next(error); });
 });
 
 module.exports = router;
