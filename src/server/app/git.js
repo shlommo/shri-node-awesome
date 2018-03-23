e21f2bc22d692cfbba1896ebb94f4c1fde27d35d
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-23 16:34:16 +0300

    получение списка коммитов по веткам

--- src/server/app/git.js
+++ src/server/app/git.js
@@ -18,12 +18,38 @@ class Git {
     });
   }
 
+  parceCommits(data) {
+    this.commits = splitLineBreaks(data);
+    return this.commits.map((item) => {
+      const commit = item.split('|');
+      return {
+        hash: commit[0],
+        message: commit[1],
+        author: commit[2],
+        date: commit[3]
+      };
+    });
+  }
+
   getAllBranches() {
     return new Promise((resolve, reject) => {
       this.exec(`cd ${this.path} && git branch --list`)
         .then((res) => {
           this.branches = this.parseBranches(res.stdout);
           resolve(this.branches);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  getBranchCommits(branch) {
+    return new Promise((resolve, reject) => {
+      this.exec(`cd ${this.path} && git log ${branch} --pretty="%h|%s|%cn|%cd" --date=short`)
+        .then((res) => {
+          const commits = this.parceCommits(res.stdout);
+          resolve(commits);
         })
         .catch((error) => {
           reject(error);
