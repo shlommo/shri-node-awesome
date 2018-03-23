a0c935fab8e3627f35a8de256a07a642e045f256
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-23 16:37:57 +0300

    роутинг отрисовки коммитов

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   app.renderAllBranches(req, res);
 });
 
+router.get('/tree/:branch', (req, res) => {
+  app.renderCommitsFromBranch(req, res);
+});
+
 module.exports = router;
